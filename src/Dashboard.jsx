35768e7a9c52c366fb697a1697aaf5a3b8a5df18
jose antonio <jsegmel@gmail.com>
2023-03-14 16:18:26 +0100

    2commit

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react"
 import GetCars from "./GetCars"
+
 export default function Dashboard (){
+    const [cars,setCars]=useState([])
    const token=localStorage.getItem('token')
-   GetCars(token)
-   .then(response=>console.log(response))
+   useEffect(()=>{
+    GetCars(token)
+   .then(response=>setCars(response))
+    
+   },[])
+
+   console.log(cars)
     return(
         <div>
-          
+          {cars.map((car)=> <div> <span>{car.marca}</span> ,<span>{car.modelo}</span> </div>)}
         </div>
     )
 }
--- src/GetCars.jsx
+++ src/GetCars.jsx
@@ -1,16 +1,14 @@
 export default function GetCars(token){
-    const unir= 'bearer'.concat('',token)
-    console.log(unir)
-    return fetch('https://x8ki-letl-twmt.n7.xano.io/api:ish37JFc/auth/login' , {
+    const unir= 'bearer'.concat(' ',token)
+    return fetch('https://x8ki-letl-twmt.n7.xano.io/api:ish37JFc/vehiculos' , {
         method: 'get',
         headers:{
-            'Authorization': unir, 
+        
+            Authorization:`${unir}`, 
         }
       })
         .then(response => response.json())
         .then(response => {
-          console.log('Success:', response)
-
           return response
         })
         .catch(error => console.error('Error:', error))
